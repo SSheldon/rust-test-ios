29931e79073f67dfd86b13024c024e0fd2060a55
Steven Sheldon <steven@sasheldon.com>
2016-04-29 22:41:46 -0700

    Link against a static lib.

--- RustTests.m
+++ RustTests.m
@@ -1,7 +1,23 @@
 #import <XCTest/XCTest.h>
 
+size_t tests_count();
+const char *test_name(size_t i, size_t *len);
+void run_test(size_t i);
+
 @interface RustTests : XCTestCase
 @end
 
 @implementation RustTests
+
+- (void)test {
+  for (size_t i = 0; i < tests_count(); i++) {
+    size_t name_len;
+    const char *c_name = test_name(i, &name_len);
+    NSString *name = [[NSString alloc] initWithBytes:c_name length:name_len encoding:NSUTF8StringEncoding];
+
+    NSLog(@"Running test: %@", name);
+    run_test(i);
+  }
+}
+
 @end
