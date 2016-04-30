bec5d84b6182ed8375e903a37e9c165e8e71bcba
Steven Sheldon <steven@sasheldon.com>
2016-04-30 14:26:17 -0700

    Expect to be run from the crate dir.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod tests;
 mod xcode;
 
 use std::env;
+use std::fs;
 
 static TESTS_PRELUDE: &'static str = r##"
 #[macro_use]
@@ -19,21 +20,23 @@ mod test_utils;
 "##;
 
 fn main() {
-    let cwd = env::current_dir().unwrap();
-    let crate_dir = cwd.parent().unwrap();
+    let crate_dir = env::current_dir().unwrap();
+
+    let build_dir = crate_dir.join("tests-ios");
+    fs::create_dir_all(&build_dir).unwrap();
 
     let src_dir = crate_dir.join("src");
     let prelude = TESTS_PRELUDE.to_owned();
-    tests::create_test_module(&cwd, &src_dir, prelude).unwrap();
+    tests::create_test_module(&build_dir, &src_dir, prelude).unwrap();
 
     let dep = cargo::Dependency {
         name: "objc",
         path: &crate_dir,
         features: &[&"exception"],
     };
-    cargo::create_config(&cwd, dep).unwrap();
-    assert!(cargo::build(&cwd).unwrap() == true);
+    cargo::create_config(&build_dir, dep).unwrap();
+    assert!(cargo::build(&build_dir).unwrap() == true);
 
-    xcode::create_project(&cwd).unwrap();
-    assert!(xcode::run_tests(&cwd).unwrap() == true);
+    xcode::create_project(&build_dir).unwrap();
+    assert!(xcode::run_tests(&build_dir).unwrap() == true);
 }
